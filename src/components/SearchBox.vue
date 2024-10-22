cc2e18f8c8bbfbed10e765638ff90c477e190cdd
Brady Dukart <dukartbrady@gmail.com>
2024-10-22 18:21:31 -0500

    new stuff

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,42 @@
+<script setup>
+import SearchBoxItem from "./SearchBoxItem.vue";
+</script>
+
+<style scoped>
+#searchBox {
+  position: absolute;
+  left: calc(50% - (35vw / 2));
+  top: 3rem;
+  width: 35vw;
+  max-width: 800px;
+  max-height: 80vh;
+  background-color: #455552;
+  border: 2px solid #52ffe2;
+  box-shadow: 0px 15px 30px #000;
+  overflow: scroll;
+  padding: 10px 0px;
+  border-radius: 18px;
+}
+</style>
+
+<template>
+  <div id="searchBox">
+    <div v-for="res in searchResults">
+      <SearchBoxItem :title="res.title" :body="res.body" :id="res.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["searchResults"],
+  created() {
+    console.log("searchResults", this.searchResults);
+  },
+  watch: {
+    searchResults: function (val) {
+      console.log("searchResults", val);
+    },
+  },
+};
+</script>
---
+++ src/components/SearchBoxItem.vue
@@ -0,0 +1,34 @@
+<style scoped>
+.searchBoxItem {
+  padding: 5px 15px;
+  display: flex;
+  flex-direction: column;
+  transition: background-color 0.25s;
+}
+.tag {
+  color: #999;
+}
+.title {
+  font-size: 1.15rem;
+}
+.searchBoxItem:hover {
+  background-color: #ff8352;
+}
+
+.searchBoxItem:hover .tag {
+  color: #000;
+}
+</style>
+
+<template>
+  <div class="searchBoxItem">
+    <span class="title">{{ title }}</span>
+    <span class="tag">ID: {{ id }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "body", "id"],
+};
+</script>
